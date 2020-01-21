3778ddce98d55ee0a99cc805630190c27cf0094e
Zeke Cato <gak500@gmail.com>
2020-01-20 18:32:37 -0600

    installed buefy and bulma

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <section class="hero">
+        <div class="hero-body tile is-ancestor">
+            <div class="tile is-parent is-11">
+                <div class="tile is-child">Zeke Cato Enterprise</div>
+                <div class="tile is-child">IT Consulting — Math, science, and technology tutoring — Sensitively Simple natural body care</div>
+            </div>
+            <div class="tile is-parent is-1">
+                <div class="tile is-child">
+                    <b-icon 
+                        pack="fas"
+                        icon="envelope"
+                        size="is-large">
+                    </b-icon>
+                </div>
+                <b-icon 
+                    pack="fab"
+                    icon="linkedin"
+                    size="is-large">
+                </b-icon>
+            </div>
+        </div>
+    </section>
+    
+</template>
+
+<script>
+export default {
+  
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,35 @@
-import BootstrapVue from 'bootstrap-vue'
+import Buefy from 'buefy'
 import '~/global.scss'
 
 //Font Awesome icons
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
-config.autoAddCss = false;
+// Buefy Icons
+import { faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle,
+  faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
+  faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload } from "@fortawesome/free-solid-svg-icons";
+library.add(faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle,
+  faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
+  faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload);
+// Site Icons
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 library.add(faLinkedin)
 library.add(faEnvelope)
+
+
+config.autoAddCss = false;
+
 
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue) {
-  Vue.use(BootstrapVue)
+  Vue.use(Buefy, {
+    defaultIconComponent: 'vue-fontawesome',
+    defaultIconPack: 'fas'
+  })
 
   Vue.component('font-awesome', FontAwesomeIcon)
   Vue.component('Layout', DefaultLayout)
