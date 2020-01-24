765cda0378bb4a41d294e21c0628dba0b22b64d6
Zeke Cato <gak500@gmail.com>
2020-01-24 14:24:31 -0600

    feeling good about responsive styling. Before vue tippy install

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,16 +1,31 @@
 <template>
-        <div class="flex flex-col sm:flex-row p-3 justify-between">
-            <div class="text-center sm:text-left">
-                <div class="text-3xl">Zeke Cato Enterprise</div>
-                <div class="text-lg">IT Consulting — Math, science, and technology tutoring — Sensitively Simple natural body care</div>
+        <div class="flex flex-col sm:flex-row p-3 justify-between h-auto">
+            <div class="text-center sm:text-left sm:w-5/6 md:w-4/6">
+                <div class="text-3xl font-semibold px-1">Zeke Cato Enterprise</div>
+                <div class="subtitleparent flex flex-wrap justify-center sm:justify-start text-lg text-gray-700">
+                    <div>
+                        IT Consulting
+                    </div>
+                    <div>:</div>
+                    <div>
+                        Math, science, and technology tutoring
+                    </div>
+                    <div>
+                        Sensitively Simple natural body care
+                    </div>
+                </div>
             </div>
-            <div class="flex flex-row justify-center sm:flex-col text-center">
-                <div class="iconlink">
+            <div class="flex flex-row justify-center items-center sm:flex-col">
+                <a href="mailto:zeke@zekecato.com" 
+                   id="envelope" 
+                   class="p-1">
                     <fontawesome :icon="['fas','envelope']" size='2x' />
-                </div>
-                <div class="iconlink">
+                </a>
+                <a href="https://www.linkedin.com/in/roy-zeke-schultz-cato-8665a453/" 
+                   id="linkedin" 
+                   class="p-1">
                     <fontawesome :icon="['fab','linkedin']" size='2x' />
-                </div>
+                </a>
             </div>
         </div>    
 </template>
@@ -20,9 +35,15 @@ export default {
   
 }
 </script>
-
+    
 <style scoped>
-    .iconlink {
-        @apply p-2;
+    .subtitleparent>*{
+        @apply px-1
+    }
+
+    @media (min-width: 640px){
+        #linkedin {
+            font-size: 1.1rem;
+        }
     }
 </style>
