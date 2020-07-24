2cdccb5a809c503d9679b9542c26f1859e98657e
Zeke Cato <gak500@gmail.com>
2020-07-24 17:13:42 -0500

    extract nav button to component. Create post list page for the list of classes.

---
+++ src/components/NavButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <g-link v-if="!!to" :to="to" class="navlink" exact-active-class="focuslink">
+    <slot />
+  </g-link>
+  <a v-else-if="!!href" :href="href" class="navlink">
+    <slot />
+  </a>
+  <div v-else>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavButton",
+  props: ["to", "href", "sublinks"],
+};
+</script>
+
+<style>
+.focuslink {
+  @apply bg-gray-700;
+}
+
+.navlink {
+  @apply py-1 mb-1 px-5 font-bold cursor-pointer select-none text-center;
+  transition: 0.2s;
+}
+
+@screen sm {
+  .navlink {
+    @apply px-4 py-3 -my-1;
+  }
+}
+.navlink:hover {
+  @apply bg-gray-600;
+}
+
+.navlink:active {
+  @apply bg-gray-800;
+}
+</style>
